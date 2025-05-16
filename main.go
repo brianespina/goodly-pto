d888b320aad6527cacdcb9e8c7985eff4bb36f1a
Brian Espina <espinabrian@gmail.com>
2025-05-16 23:57:06 +0800

    added Auth logic

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"fmt"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
@@ -22,15 +24,27 @@ type Role struct {
 	Title string
 }
 
-func AuthRequired() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		token := c.GetHeader("rsietna")
-		if token != "secret123" { // Replace this with real validation
-			c.Redirect(http.StatusFound, "/login")
+func AuthRequired(db *pgx.Conn) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		session_id, err := ctx.Cookie("session_id")
+		var user_id int
+		if err != nil {
+			fmt.Println("no session ID in cookies")
+			ctx.Redirect(http.StatusSeeOther, "/login")
+			ctx.Abort()
 			return
 		}
-		// Continue to the next handler
-		c.Next()
+		uuid_session_id, err := uuid.Parse(session_id)
+		err = db.QueryRow(ctx, "SELECT user_id FROM sessions WHERE id = $1 AND expires_at > NOW()", uuid_session_id).Scan(&user_id)
+		if err != nil {
+			fmt.Println("no session in DB")
+			fmt.Println(err)
+			ctx.Redirect(http.StatusSeeOther, "/login")
+			ctx.Abort()
+			return
+		}
+		ctx.Set("user_id", user_id)
+		ctx.Next()
 	}
 }
 
@@ -38,6 +52,7 @@ func main() {
 
 	godotenv.Load()
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DBSTRING"))
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
@@ -50,7 +65,7 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 
 	auth := r.Group("/")
-	auth.Use(AuthRequired())
+	auth.Use(AuthRequired(conn))
 	auth.GET("/users", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "users.html", nil)
 	})
@@ -80,7 +95,9 @@ func main() {
 		password := ctx.PostForm("password")
 		passwordcopy := ctx.PostForm("passwordcopy")
 		if password != passwordcopy {
-			fmt.Println("Passwords did not match")
+			ctx.HTML(http.StatusOK, "register.html", gin.H{
+				"flash": "Passwords did not match",
+			})
 			return
 		}
 		hash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -100,11 +117,12 @@ func main() {
 		stdCtx := ctx.Request.Context()
 		var email string
 		var hashed_password *string
+		var id int
 
 		form_email := ctx.PostForm("email")
 		form_password := ctx.PostForm("password")
 
-		err := conn.QueryRow(stdCtx, "SELECT email, password FROM users WHERE email = $1", form_email).Scan(&email, &hashed_password)
+		err := conn.QueryRow(stdCtx, "SELECT email, password, id FROM users WHERE email = $1", form_email).Scan(&email, &hashed_password, &id)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -112,9 +130,21 @@ func main() {
 
 		err = bcrypt.CompareHashAndPassword([]byte(*hashed_password), []byte(form_password))
 		if err != nil {
+			// TODO: Handle wrong password
 			fmt.Println(err)
 			return
 		}
+
+		// TODO: Login logic here
+		session_id := uuid.New().String()
+		expires := time.Now().Add(24 * time.Hour)
+
+		_, err = conn.Exec(ctx, "INSERT INTO sessions (id, user_id, expires_at) VALUES ($1, $2, $3)", session_id, id, expires)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		ctx.SetCookie("session_id", session_id, 86400, "/", "localhost", false, true)
 
 		fmt.Println("Log me in")
 
